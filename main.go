9e29c73ed8ac0490e10dd0b8463ff2861c50cfac
Saul Pinales <saul021002@gmail.com>
2021-03-25 18:01:07 -0400

    help

--- main.go
+++ main.go
@@ -4,20 +4,36 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
 	u := flag.Bool("u", false, "Write bytes from the input file to the standard output without delay as each is read.")
-	ver := flag.Bool("version", false, "output version information and exit")
+	ver := flag.Bool("version", false, "output version information and exit.")
+	help := flag.Bool("help", false, "display this help and exit.")
 	flag.Parse()
 
 	if *ver {
-		fmt.Println("0.0.1")
+		version()
+	}
+
+	if *help {
+		options()
 	}
 
 	for _, f := range flag.Args() {
 		cat(f, *u)
 	}
+}
+
+func options() {
+	flag.PrintDefaults()
+	os.Exit(0)
+}
+
+func version() {
+	fmt.Println("0.0.1")
+	os.Exit(0)
 }
 
 func cat(filename string, u bool) {
